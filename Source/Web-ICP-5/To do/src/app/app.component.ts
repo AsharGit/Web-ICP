9e2074ce721a3709b09a474df31efcf8cd2285cc
AsharGit <32973667+AsharGit@users.noreply.github.com>
2021-02-20 22:59:14 -0600

    ICP update

---
+++ Source/Web-ICP-5/To do/src/app/app.component.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  // define list of items
+  items= [];
+
+  // Write code to push new item
+  submitNewItem(value) {
+    if(value!==""){
+      this.items.push(value)
+    }
+    else{
+      alert('Please enter a task')
+    }
+  }
+
+  // Write code to complete item
+  completeItem() {
+
+
+  }
+
+  // Write code to delete item
+  deleteItem(value) {
+    for(let i=0 ;i<= this.items.length ;i++){
+      if(value == this.items[i]){
+        this.items.splice(i,1)
+      }
+    }
+
+  }
+
+}
